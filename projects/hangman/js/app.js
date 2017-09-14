c9d01c5a6f4ec4b4784c26ce85078f04a769cfe1
BradOFarrell <bradofarrell@gmail.com>
2017-09-13 21:19:29 -0400

    Added an array of letters, and functionality to choose and remove a letter

--- projects/hangman/js/app.js
+++ projects/hangman/js/app.js
@@ -1,3 +1,22 @@
+const GameLogic = {
+    availableLetters: "QWERTYUIOPASDFGHJKLZXCVBNM",
+    guessLetter: function(index){
+        let selectedLetter = GameLogic.availableLetters[index];
+        let output = "";
+        output += GameLogic.availableLetters.slice(0,index);
+        output += GameLogic.availableLetters.slice(index+1,GameLogic.availableLetters.length);
+        GameLogic.availableLetters = output;
+    }
+}
+
+const GameUI = {
+
+}
+    
 $(function(){ // Main, waits for dom to load
-    $('#buttonsBox').append("<br>lksdnlkfndd");
+    GameLogic.guessLetter(2);
+    let tempLetters = GameLogic.availableLetters;
+    for (let i = 0; i < tempLetters.length; i++){
+        $('#buttonBox').append(tempLetters[i]);        
+    }
 });
