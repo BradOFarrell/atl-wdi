3be5350d888a75a355f4763dbbe68ca4ecd4b4a7
BradOFarrell <bradofarrell@gmail.com>
2017-09-15 12:32:48 -0400

    Fixed loading issues

--- projects/hangman/js/app.js
+++ projects/hangman/js/app.js
@@ -1,50 +1,61 @@
 const GameLogic = {
-    possibleAnswers:["Boardwalk_Empire","Breaking_Bad","Downton_Abbey","Homeland","The_Newsroom","The_Good_Wife","Mad_Men","Veep","Thirty_Rock","Parks_and_Recreation","Girls","The_Handmaidens_Tale","The_Big_Bang_Theory","New_Girl","Sesame_Street","Smash","Modern_Family","Sherlock","Seinfeld","Glee","The_OC","The_Simpsons","Family_Guy","King_of_the_Hill","The_Office","Buffy","Community","The_Good_Place","Scrubs","Iron_Chef"],
+    possibleAnswers:["Boardwalk_Empire","Breaking_Bad","Downton_Abbey","Homeland","The_Newsroom","The_Good_Wife","Mad_Men","Veep","Thirty_Rock","Parks_and_Recreation","Girls","The_Handmaids_Tale","The_Big_Bang_Theory","New_Girl","Sesame_Street","Smash","Modern_Family","Sherlock","Seinfeld","Glee","The_OC","The_Simpsons","Family_Guy","King_of_the_Hill","The_Office","Buffy","Community","The_Good_Place","Scrubs","Iron_Chef"],
     correctAnswer: "",
     currentGuess: "",
     mistakes: 0,
+    isGameRunning: true, // Becomes false when a win or lose state is activated
     availableLetters: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
     guessLetter: function(guessIndex){
-        let selectedLetter = GameLogic.availableLetters[guessIndex];
-        let remainingLetters = "";
-        let isGuessWrong = true; // Changed to false when a correct guess is made 
-
-        // Choose letter, remove it from available remaining letters list
-        remainingLetters += GameLogic.availableLetters.slice(0,guessIndex);
-        remainingLetters += GameLogic.availableLetters.slice(guessIndex+1,GameLogic.availableLetters.length);
-        GameLogic.availableLetters = remainingLetters;
-
-        // Compare guess to answer, reveal if found
-        for(let i = 0; i < GameLogic.correctAnswer.length; i++){
-            console.log(GameLogic.correctAnswer[i].toUpperCase() +" "+ selectedLetter.toUpperCase())
-            if(GameLogic.correctAnswer[i].toUpperCase() == selectedLetter.toUpperCase()){
-                let newGuessText = "";
-                // Update the current guess string to include correct letter
-                newGuessText += GameLogic.currentGuess.slice(0,i);
-                newGuessText += GameLogic.correctAnswer[i]; 
-                newGuessText += GameLogic.currentGuess.slice(i+1,GameLogic.currentGuess.length);
-                GameLogic.currentGuess = newGuessText
-
-                GameUI.renderClue();
-                isGuessWrong = false;
-            } 
-        }
-
-        // Incriment mistake counter, check for loss
-        if(isGuessWrong){
-            GameLogic.mistakes++;            
-        }
-
-        // Check for loss and win conditions
-        if(GameLogic.mistakes > 5){
-            alert("You lose!");
-            location.reload();
-        } else if(GameLogic.currentGuess == GameLogic.correctAnswer){
-            alert("You win!");
-            location.reload();            
-        } else {
+        if(GameLogic.isGameRunning){
+            let selectedLetter = GameLogic.availableLetters[guessIndex];
+            let remainingLetters = "";
+            let isGuessWrong = true; // Changed to false when a correct guess is made 
+    
+            // Choose letter, remove it from available remaining letters list
+            remainingLetters += GameLogic.availableLetters.slice(0,guessIndex);
+            remainingLetters += GameLogic.availableLetters.slice(guessIndex+1,GameLogic.availableLetters.length);
+            GameLogic.availableLetters = remainingLetters;
+    
+            // Compare guess to answer, reveal if found
+            for(let i = 0; i < GameLogic.correctAnswer.length; i++){
+                console.log(GameLogic.correctAnswer[i].toUpperCase() +" "+ selectedLetter.toUpperCase())
+                if(GameLogic.correctAnswer[i].toUpperCase() == selectedLetter.toUpperCase()){
+                    let newGuessText = "";
+                    // Update the current guess string to include correct letter
+                    newGuessText += GameLogic.currentGuess.slice(0,i);
+                    newGuessText += GameLogic.correctAnswer[i]; 
+                    newGuessText += GameLogic.currentGuess.slice(i+1,GameLogic.currentGuess.length);
+                    GameLogic.currentGuess = newGuessText
+    
+                    isGuessWrong = false;
+                } 
+            }
+    
+            // Incriment mistake counter, check for loss
+            if(isGuessWrong){
+                GameLogic.mistakes++;            
+            }
+    
+            // Render changes to game state made by the guess
             GameUI.renderAll();            
+            
+            // Check for loss and win conditions
+            if(GameLogic.mistakes >= 5){
+                GameLogic.isGameRunning = false; // Ensures player won't interact with game during render
+                setTimeout(GameLogic.loseGame, 1000); // SetTimeOut to give DOM time to render
+            } else if(GameLogic.currentGuess == GameLogic.correctAnswer){
+                GameLogic.isGameRunning = false;  // Ensures player won't interact with game during render
+                setTimeout(GameLogic.winGame, 1000); // SetTimeOut to give DOM time to render
+            }                
         }
+    },
+    loseGame: function(){
+        alert("You lose! The correct anwser was: "+GameLogic.correctAnswer);
+        location.reload();
+    },
+    winGame: function() {
+        alert("You win!");
+        location.reload();      
     },
     startGame: function() {
         let randomAnswerIndex = Math.floor(Math.random() * GameLogic.possibleAnswers.length);
