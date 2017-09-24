31ab65444a733c4fd8717b45b1f152a0b445d754
BradOFarrell <bradofarrell@gmail.com>
2017-09-24 19:18:15 -0400

    Added start and end conditions to tamagochi

--- homework/javaScript/js-constructor/player.js
+++ homework/javaScript/js-constructor/player.js
@@ -3,40 +3,29 @@ console.log('player file is loaded');
 class Player {
   constructor(name){
     this.name = name;
+    console.log("Hello, "+name+"!");
+    console.log("Use any of the following commands:");
+    console.log(" p.feed(mon); <-- Raises food stat");
+    console.log(" p.heal(mon); <-- Raises health stat");
+    console.log(" p.tuck(mon); <-- Raises rest stat");   
   }
   feed(tamagotchi){
-    console.log("OmNomOm | "+player.name+ " feeds "+tamagotchi.name+" snacks. [food +2]");
+    console.log("OmNomOm | "+this.name+ " feeds "+tamagotchi.name+" snacks. [food +2]");
     tamagotchi.food += 2;
     console.log(tamagotchi.getStatus());
   }
   heal(tamagotchi){
-    console.log(" * <3 * | "+player.name+ " kisses "+tamagotchi.name+"'s wounds. [health +2]");
+    console.log(" * <3 * | "+this.name+ " kisses "+tamagotchi.name+"'s wounds. [health +2]");
     tamagotchi.health += 2;
     console.log(tamagotchi.getStatus());
   }
   tuck(tamagotchi){
-    console.log(".zZzZz. | "+player.name+ " tucks "+tamagotchi.name+" into bed. [rest +2]");
+    console.log(".zZzZz. | "+this.name+ " tucks "+tamagotchi.name+" into bed. [rest +2]");
     tamagotchi.rest += 2;
     console.log(tamagotchi.getStatus());
   }
 }
 
 //create new Tamagotchis
-let monster1 = new Tamagotchi("Tommy", "XL");
-let monster2 = new Tamagotchi("Gotchy", "XL");
-let player = new Player("Brad")
-
-//create new Tamagotchis
-console.log(monster1);
-console.log(monster2);
-
-//Invoke methods on the constructed objects:
-monster1.cry();
-monster2.puke();
-
-//test out your Tamagotchies below via console.logs
-player.feed(monster2);
-monster2.cry();
-monster2.puke();
-player.heal(monster2);
-player.tuck(monster2);
+let p = new Player("Brad")
+let mon = new Tamagotchi("Tommy", "XL");
--- homework/javaScript/js-constructor/tamagotchi.js
+++ homework/javaScript/js-constructor/tamagotchi.js
@@ -2,54 +2,82 @@ console.log('tamagotchi file is loaded');
 
 //your class declaration here
 class Tamagotchi {
-    constructor(name, type){
-        this.name = name;
-        this.eyes = name.toString().charAt(0).toUpperCase(); // Takes name to generate eyes        
-        this.type = type; 
-        this.food = 10;
-        this.rest = 10;
-        this.health = 10;
-    };
-    getStatus(){
-        let outputStatus = this.getFace(); // Puts face at top of status
-        outputStatus += " | "+this.name+": ";
-        outputStatus += "[Food: "+this.food+" ";
-        outputStatus += "| Rest: "+this.rest+" ";
-        outputStatus += "| Health: "+this.health+"] ";
-        outputStatus += "Type: "+this.type+"";
-        return outputStatus;
-    };
-    getFace(){
-        let outputFace = "<(O_O)>"; // Default face, will change later
-        if(this.health < 3){
-            outputFace = "<(X_X)>";
-        } else if (this.food < 3){
-            outputFace = "\\(‾o‾)/";
-        } else if (this.rest < 3){
-            outputFace = "/(ó_ò)\\";
-        } else if (this.health > 10 && this.rest > 10) {
-            outputFace = "<(^_^)>";
-        } else {
-            outputFace = "<("+this.eyes+"_"+this.eyes+")>";
-        }
-        return outputFace;
-    };
-    cry(){
-        this.food--;
-        this.rest--;
-        console.log("<(T~T)> | "+this.name+" is crying!!! \"Waahh!\" [Food -1] [Rest -1]");
-        console.log(this.getStatus());
-    };
-    puke(){
-        this.food -= 2;
-        this.health--;
-        console.log("(>‾o‾)> | "+this.name+" is puking!!! \"Blehh!\" [Food -2] [health -1]");
-        console.log(this.getStatus());
-    };
-    yawn(){
-        this.food -= 2;
-        this.health--;
-        console.log("<(^o^<) | "+this.name+" is yawning... \"Sigh!\" [Food -1] [Rest -2]");
-        console.log(this.getStatus());
-    };
-}    
+constructor(name, type){
+    this.name = name;
+    this.eyes = name.toString().charAt(0).toUpperCase(); // Takes name to generate eyes        
+    this.type = type; 
+    this.food = 10;
+    this.rest = 10;
+    this.health = 10;
+    this.start();
+};
+getStatus(){
+    let outputStatus = this.getFace(); // Puts face at top of status
+    outputStatus += " | "+this.name+": ";
+    outputStatus += "[Food: "+this.food+" ";
+    outputStatus += "| Rest: "+this.rest+" ";
+    outputStatus += "| Health: "+this.health+"] ";
+    outputStatus += "Type: "+this.type+"";
+    return outputStatus;
+};
+getFace(){
+    let outputFace = "<(O_O)>"; // Default face, will change later
+    if(this.health < 3){
+        outputFace = "<(X_X)>";
+    } else if (this.food < 3){
+        outputFace = "\\(‾o‾)/";
+    } else if (this.rest < 3){
+        outputFace = "/(ó_ò)\\";
+    } else if (this.health > 10 && this.rest > 10) {
+        outputFace = "<(^_^)>";
+    } else {
+        outputFace = "<("+this.eyes+"_"+this.eyes+")>";
+    }
+    return outputFace;
+};
+cry(){
+    this.food--;
+    this.rest--;
+    console.log("<(T~T)> | "+this.name+" is crying!!! \"Waahh!\" [Food -1] [Rest -1]");
+    console.log(this.getStatus());
+    this.checkCondition();
+};
+yawn(){
+    this.food -= 2;
+    this.health--;
+    console.log("<(^o^<) | "+this.name+" is yawning... \"Sigh!\" [Food -1] [Rest -2]");
+    console.log(this.getStatus());
+    this.checkCondition();
+};
+puke(){
+    this.food -= 2;
+    this.health--;
+    console.log("(>‾o‾)> | "+this.name+" is puking!!! \"Blehh!\" [Food -2] [health -1]");
+    console.log(this.getStatus());
+    this.checkCondition();
+};
+checkCondition(){
+    if(this.health <= 0){
+        this.stop();
+    }
+}
+start(){
+    console.log(".('-'). | "+this.name+" is hatching...");
+    var self = this;
+    this.hungerTimer = setInterval(function() {
+        self.cry();
+    }, 6000);
+    this.yawnTimer = setInterval(function() {
+        self.yawn();
+    }, 10000);
+    this.sickTimer = setInterval(function() {
+        self.puke();
+    }, 25000);
+};
+stop(){
+    console.log(".[RIP]. | "+this.name+" closes its eyes...");
+    clearInterval(this.hungerTimer);
+    clearInterval(this.yawnTimer);
+    clearInterval(this.sickTimer);
+};
+}
