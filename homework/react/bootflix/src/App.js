7ff413bef5f54dfe57ca55b67bf9a05015c82c9c
BradOFarrell <bradofarrell@gmail.com>
2017-11-22 20:00:07 -0500

    Pulled movie from API and displayed it in component

--- homework/react/bootflix/src/App.js
+++ homework/react/bootflix/src/App.js
@@ -2,19 +2,32 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 import Search from './components/Search';
 import Movie from './components/Movie';
-import example from './omdbExample.json'
+import example from './omdbExample.json';
+import axios from 'axios';
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
-      movie: example
+      movie: example,
+      apiurl: "http://img.omdbapi.com/?apikey=d31f1a94&"
     }
   }
 
+  // http://img.omdbapi.com/?i=tt2294629&apikey=d31f1a94 
   //Update these methods to make axios calls to OMDB and update this.state.movie with the response from the server
-  _searchByTitle = () => {
-    console.log("Search by Title");
+  _searchByTitle = (title) => {
+    let searchURL = "http://www.omdbapi.com/?i=tt3896198&apikey=d36d1964"
+    console.log(searchURL)
+    axios.get(searchURL).then((res) => {
+      console.log(res.data);
+      this.setState({movie: res.data})
+    })
+    .catch((err) => {
+      console.log("Error loading users. "+err);
+    })
+
+    console.log("Search by Title: "+title);
   }
 
   _searchById = () => {
@@ -22,12 +35,15 @@ class App extends Component {
   }
 
   //Pass _searchByTitle, _searchById, and this.state.movie to it's appropriate child components.
+  componentDidMount(){
+    this._searchByTitle("Spiderman");
+  }
   render() {
     return (
       <div className="App">
         <Header />
         <Search />
-        <Movie />
+        <Movie movie={this.state.movie}/>
       </div>
     );
   }
